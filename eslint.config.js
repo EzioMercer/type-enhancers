b3b1035dba705f26b410e26a76f587a227a3bf9c
EzioMercer <rovsanbadirxanov@gmail.com>
2025-06-12 02:54:36 +0400

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default defineConfig([
+    {
+        files: ['**/*.*'],
+        languageOptions: {
+            parser: tsparser,
+        },
+        plugins: {
+            '@stylistic': stylistic
+        },
+        rules: {
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/eol-last': ['error', 'always'],
+            '@stylistic/linebreak-style': ["error", "unix"],
+            '@stylistic/semi': ["error", "always"]
+        }
+    }
+]);
